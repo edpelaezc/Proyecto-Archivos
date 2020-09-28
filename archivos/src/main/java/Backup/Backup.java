c6fb4cff3303f30d409166e975d1b9b22529af1f
Emmanuel Alvarado <josue0ghost@gmail.com>
2020-09-27 18:25:49 -0600

    Documentación de clases Backup

--- archivos/src/main/java/Backup/Backup.java
+++ archivos/src/main/java/Backup/Backup.java
@@ -14,12 +14,22 @@ public class Backup {
     private String usuario;
     private String fecha_transaccion;
     
+    /**
+     * Constructor de objeto Backup
+     * @param ruta_absoulta ruta destino del backup
+     * @param usuario nombre de usuario del administrador
+     * @param fecha_transaccion fecha actual
+     */
     public Backup(String ruta_absoulta, String usuario, String fecha_transaccion){
         this.ruta_absoluta = ruta_absoluta;
         this.usuario = usuario;
         this.fecha_transaccion = fecha_transaccion;
     }
     
+    /**
+     * Método toString de la clase Backup
+     * @return retorna el objeto con un formato separado por "|"
+     */
     public String toString(){
         return this.ruta_absoluta + "|" + this.usuario + "|" + this.fecha_transaccion;
     }
