a66fa1659cbd0feffce22f160581cec7aa110717
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-10-25 12:05:06 -0600

    Create HandleContacts.java

---
+++ archivos/src/main/java/Contacts/HandleContacts.java
@@ -0,0 +1,13 @@
+package Contacts;
+
+import java.io.File;
+
+public class HandleContacts {
+    File contactos = new File("C:\\MEIA\\contactos.txt");
+    File desc_contactos = new File("C:\\MEIA\\desc_contactos.txt");
+
+
+
+
+
+}
