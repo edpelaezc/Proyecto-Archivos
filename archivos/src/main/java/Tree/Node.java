7cc1cf247e780d0d89d7a6467ce8294f31c0d8b6
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-11-13 23:07:00 -0600

    Update Node.java

--- archivos/src/main/java/Tree/Node.java
+++ archivos/src/main/java/Tree/Node.java
@@ -9,6 +9,32 @@ package Tree;
  *
  * @author edanP
  */
-public class Node {
-    
+public class Node<Correo> {
+
+    private Correo element;
+    private Node<Correo> next;
+
+    /**
+     * Objeto nodo utilizado para orden del árbol
+     *
+     * @param newElement
+     * @param nextNode
+     */
+    public Node(Correo newElement, Node<Correo> nextNode) {
+        element = newElement;
+        next = nextNode;
+    }
+
+    public Correo getElement() {
+        return element;
+    }
+
+    public Node<Correo> getNext() {
+        return next;
+    }
+
+    public void setNext(Node<Correo> next) {
+        this.next = next;
+    }
+
 }
